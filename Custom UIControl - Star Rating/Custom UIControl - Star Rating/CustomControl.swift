93f7d733f39fa6288eb7fa92e40482a7498e3098
fritzgt <tuanis@live.com>
2020-02-17 08:02:46 +0200

    Initial commit

---
+++ Custom UIControl - Star Rating/Custom UIControl - Star Rating/CustomControl.swift
@@ -0,0 +1,15 @@
+//
+//  CustomControl.swift
+//  Custom UIControl - Star Rating
+//
+//  Created by FGT MAC on 2/17/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+
+class CustomControl: UIControl {
+    var value: Int = 1
+    
+}
---
+++ Custom UIControl - Star Rating/Custom UIControl - Star Rating/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Custom UIControl - Star Rating
+//
+//  Created by FGT MAC on 2/17/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var updateRating: CustomControl!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
